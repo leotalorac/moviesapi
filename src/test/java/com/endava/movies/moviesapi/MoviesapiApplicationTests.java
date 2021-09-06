fc6da0bc2363d1f117c5fb92a9f72be6e930e954
leotalorac <leotalorac@unal.edu.co>
2021-09-06 12:40:42 -0500

    initial commit

---
+++ src/test/java/com/endava/movies/moviesapi/MoviesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endava.movies.moviesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
